2c4ea41be2e87181c3539aab0e58f23b5794cce2
Dimos Merachtsakis <dimosmera@gmail.com>
2023-05-24 19:05:56 +0300

    adds generate_embeddings function and removes the openai class

---
+++ scripts/utils/generate_embeddings.rb
@@ -0,0 +1,33 @@
+require 'httparty'
+
+require_relative '../environment'
+
+# Returns an array of embedding vectors
+def generate_embeddings(text)
+  body = {
+    'input' => text,
+    'model' => 'text-embedding-ada-002'
+  }
+  headers = {
+    'Content-Type' => 'application/json',
+    'Authorization' => "Bearer #{ENV['OPENAI_API_KEY']}"
+  }
+
+  result = HTTParty.post(
+    'https://api.openai.com/v1/embeddings',
+    body: body.to_json,
+    headers: headers
+  )
+
+  if result.success?
+    puts result.parsed_response['usage']
+
+    result.parsed_response['data']
+  else
+    puts "Failed to generate embeddings #{result.parsed_response}"
+    []
+  end
+rescue HTTParty::Error => e
+  puts "Error generating embeddings: #{e.message}"
+  []
+end
