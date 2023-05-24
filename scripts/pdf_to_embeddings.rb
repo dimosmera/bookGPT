ebc04fe8ce0d9dfec9fb3d53f1f86e80d8857f0e
Dimos Merachtsakis <dimosmera@gmail.com>
2023-05-24 19:11:06 +0300

    writes embeddings to local data file

--- scripts/pdf_to_embeddings.rb
+++ scripts/pdf_to_embeddings.rb
@@ -1,9 +1,19 @@
-require_relative 'extract_chunks'
-require_relative 'OpenAI'
+require 'csv'
 
-openai_api = OpenAI.new
+require_relative 'utils/extract_chunks'
+require_relative 'utils/generate_embeddings'
 
 chunks = extract_chunks
 
-embeddings = openai_api.embeddings(chunks)
-puts "embeddings: #{embeddings}"
+embeddings = chunks.map do |chunk|
+  { text: chunk, embedding: generate_embeddings(chunk)[0]['embedding'] }
+end
+
+# Writes a CSV with exactly these named columns: "content", "embedding"
+CSV.open('book_embeddings.csv', 'w') do |csv|
+  csv << %w[content embedding]
+
+  embeddings.each do |embedding|
+    csv << [embedding[:text], embedding[:embedding]]
+  end
+end
