e46e6e3af149eb38a7e645c6abf41ca837cfc2c8
Dimos Merachtsakis <dimosmera@gmail.com>
2023-05-25 18:55:07 +0300

    reads embeddings from local csv within the lambda

--- backend/app/handler.rb
+++ backend/app/handler.rb
@@ -1,6 +1,19 @@
 require 'json'
+require 'csv'
 
 def ask(event:, context:)
+  embeddings_from_csv = []
+
+  # Reads the local CSV file
+  CSV.foreach('embeddings/i_have_no_mouth_and_i_must_scream.csv', headers: true, header_converters: :symbol) do |row|
+    embeddings_from_csv << { text: row[:content], embedding: row[:embedding] }
+  end
+
+  embeddings_from_csv.each do |embedding|
+    puts embedding[:text]
+    puts embedding[:embedding]
+  end
+
   {
     statusCode: 200,
     body: {
