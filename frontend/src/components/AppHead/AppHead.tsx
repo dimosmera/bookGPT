42b4b38ff8d0cdc1a43c6162f717a8af8e8c1cfe
Dimos Merachtsakis <dimosmera@gmail.com>
2023-05-26 20:51:56 +0300

    changes favicon and pagea title

--- frontend/src/components/AppHead/AppHead.tsx
+++ frontend/src/components/AppHead/AppHead.tsx
@@ -3,13 +3,13 @@ import NextHead from "next/head";
 const AppHead = () => {
   return (
     <NextHead>
-      <title>Ask A Book</title>
+      <title>Ask A Book: I Have No Mouth, and I Must Scream</title>
       <meta
         name="description"
         content="Ask questions to a book. Powered by ChatGPT"
       />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <link rel="icon" href="/favicon.ico" />
+      <link rel="icon" href="/favicon.svg" />
     </NextHead>
   );
 };
